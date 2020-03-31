ce251801f8398dc44bde87895d510485458ef5a8
Srijan <srijan.manandhar@gmail.com>
2020-03-31 19:53:37 +1100

    Used hook useStaticQuery to fetch site title using graphql query in component

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,9 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `BlazinFastBlog`,
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-typography`,
--- src/components/container.js
+++ src/components/container.js
@@ -1,20 +1,33 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import containerStyles from './container.module.css'
 import ListLink from './listlink'
 
-export default ({children}) => (
-  <div className={containerStyles.container}>
-    <header style={{ marginBottom: `1.5rem`}}>
-      <Link to='/' style={{ textShadow: `none`, backgroundImage: `none`}}>
-        <h3 style={{display: `inline`}}>BlazinFastBlog</h3>
-      </Link>
-      <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>
-    {children}
-  </div>
-)
+export default (props) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div className={containerStyles.container}>
+      <header style={{ marginBottom: `1.5rem`}}>
+        <Link to='/' style={{ textShadow: `none`, backgroundImage: `none`}}>
+          <h3 style={{display: `inline`}}>{data.site.siteMetadata.title}</h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {props.children}
+    </div>
+  )
+}
